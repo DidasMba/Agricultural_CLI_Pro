4676b6bf9e9f9b7f708bf05d11e090947921d21d
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-14 13:06:30 +0300

    sent

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+# main.py
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.models import Base
+
+# Connect to the database
+engine = create_engine('sqlite:///educational_system.db', echo=True)  # Change the database URL as needed
+
+# Create tables
+Base.metadata.create_all(bind=engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Close the session when done (optional)
+# session.close()
