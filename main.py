e55112b8cda194f9cad9762dd0ae17acdb5375ae
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-16 14:55:36 +0300

    completed

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from datetime import datetime
 # Connect to the database
 engine = create_engine('sqlite:///educational_system.db', echo=True)  # Change the database URL as needed
 
-# Create tables
 Base.metadata.create_all(bind=engine)
 
 # Create a session to interact with the database
